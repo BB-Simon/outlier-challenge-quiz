82e19d09581cfcfcb00e212b736391d53b84b899
BB-Simon <simon.chowdery@gmail.com>
2022-10-21 23:05:19 +0200

    Updates

---
+++ precache-manifest.d73215a9314bf3fea6f3e03290ac49e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55478c68e4afdd4e2d5d6468aca7b186",
+    "url": "/BB-Simon.github.io/outlier-challenge-quiz/index.html"
+  },
+  {
+    "revision": "7171a6683fbd50c2bcd3",
+    "url": "/BB-Simon.github.io/outlier-challenge-quiz/static/css/main.3a6220a9.chunk.css"
+  },
+  {
+    "revision": "048a79095d8b821b8bd8",
+    "url": "/BB-Simon.github.io/outlier-challenge-quiz/static/js/2.fcefab13.chunk.js"
+  },
+  {
+    "revision": "7171a6683fbd50c2bcd3",
+    "url": "/BB-Simon.github.io/outlier-challenge-quiz/static/js/main.28f72af2.chunk.js"
+  },
+  {
+    "revision": "6a30c5f382d398d2c3c9",
+    "url": "/BB-Simon.github.io/outlier-challenge-quiz/static/js/runtime~main.2f1d9466.js"
+  }
+]);
